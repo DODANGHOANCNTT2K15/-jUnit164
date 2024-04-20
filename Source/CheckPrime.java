ef6f072df4edf7aa4822ff8d73678fc6f0a65380
DODANGHOANCNTT2K15 <dodanghoana12017@gmail.com>
2024-04-20 23:55:51 +0700

    CheckPrime

---
+++ Source/CheckPrime.java
@@ -0,0 +1,15 @@
+package Source;
+
+public class CheckPrime {
+    public boolean CheckNumber(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
